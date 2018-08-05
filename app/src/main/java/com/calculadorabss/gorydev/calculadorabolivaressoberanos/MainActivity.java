e3598aa50f826aca17ca5b67bf198c587a9a9498
Carlos <carlosd.dev@gmail.com>
2018-08-05 19:07:36 -0400

    Subiendo el proyecto en su version 1.1
    Signed-off-by: Carlos <carlosd.dev@gmail.com>

---
+++ app/src/main/java/com/calculadorabss/gorydev/calculadorabolivaressoberanos/MainActivity.java
@@ -0,0 +1,65 @@
+package com.calculadorabss.gorydev.calculadorabolivaressoberanos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText editCantidad;
+    public Button btnCalcular, btnLimpiar;
+    public TextView txtResultado;
+    public final double CONVERSION = 100000;
+    private static DecimalFormat REAL_FORMATTER = new DecimalFormat("###,###.##");//Dar formato al texto de salida
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editCantidad = findViewById(R.id.editMonto);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnLimpiar = findViewById(R.id.btnLimpiar);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convertirMonto();
+            }
+        });
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limpiarText();
+            }
+        });
+        editCantidad.addTextChangedListener(new NumberTextWatcher(editCantidad));
+
+    }
+
+    private void limpiarText() {
+        editCantidad.setText("");
+        txtResultado.setText("");
+    }
+
+    public void convertirMonto(){
+        double monto = Double.parseDouble(editCantidad.getText().toString().replace(",",""));
+        double resultado = monto /= CONVERSION;
+        String salida = "";
+        //txtResultado.setText(String.valueOf(resultado));
+        salida = "Son: " + REAL_FORMATTER.format((resultado)) + " BsS.";
+        txtResultado.setText(salida);
+    }
+
+}
+
+
+
