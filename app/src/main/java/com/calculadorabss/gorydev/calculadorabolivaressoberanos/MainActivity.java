04f83d9881f019c314cfdb67261a5fbd466c78c3
Carlos <carlosd.dev@gmail.com>
2018-08-10 19:16:53 -0400

    --Version 1.1.1 Corregido BUG que provocaba el cierre de la aplicacion cuando se daba click a calcular sin haber introducido una cantidad.
    Signed-off-by: Carlos <carlosd.dev@gmail.com>

--- app/src/main/java/com/calculadorabss/gorydev/calculadorabolivaressoberanos/MainActivity.java
+++ app/src/main/java/com/calculadorabss/gorydev/calculadorabolivaressoberanos/MainActivity.java
@@ -2,6 +2,7 @@ package com.calculadorabss.gorydev.calculadorabolivaressoberanos;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -50,15 +51,21 @@ public class MainActivity extends AppCompatActivity {
         txtResultado.setText("");
     }
 
-    public void convertirMonto(){
-        double monto = Double.parseDouble(editCantidad.getText().toString().replace(",",""));
-        double resultado = monto /= CONVERSION;
-        String salida = "";
-        //txtResultado.setText(String.valueOf(resultado));
-        salida = "Son: " + REAL_FORMATTER.format((resultado)) + " BsS.";
-        txtResultado.setText(salida);
+    public void convertirMonto() {
+        if (!editCantidad.getText().equals("")) {
+            try {
+                double monto = Double.parseDouble(editCantidad.getText().toString().replace(",", ""));
+                double resultado = monto /= CONVERSION;
+                String salida = "";
+                //txtResultado.setText(String.valueOf(resultado));
+                salida = "Son: " + REAL_FORMATTER.format((resultado)) + " Bs.S";
+                txtResultado.setText(salida);
+            } catch (NumberFormatException e) {
+                //Log.e("Error-->", "convertirMonto: " + e.getMessage());
+                Toast.makeText(this, "Por favor indique una cantidad para calcular", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-
 }
 
 
